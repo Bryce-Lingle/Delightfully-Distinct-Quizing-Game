86031b7c9a2defadca03be2f2e064891b26d71e6
BryceLingle <brycealingle@gmail.com>
2023-05-23 23:27:33 -0600

    HTML file starting to look how I want it

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,1 @@
 //I want question cards -- so I am going to build them here
-var questions = [{
-    id: 0, 
-        q: "Who is the coolest person in our family?"
-        a:  [{ text: "me", iscorrect: true}
-        
-    ]
-}
-
-]
