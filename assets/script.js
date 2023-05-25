4047a365f28feda0d51fc7ffb639f186f8688131
BryceLingle <brycealingle@gmail.com>
2023-05-24 21:04:45 -0600

    js start with notes on how I envision what I need

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
-//I want question cards -- so I am going to build them here
+//What do I need? 
+//function for starting
+//function for selecting the answer
+//fucnction for next question
+//function for timer
+//function for cards to randomize on browser load
+
+function startQuiz(){
+//I need the hide elements to be undone which I will need to research
+//I need the timer to start
+//I need to be presented with the first question
+
+}
+
+function selectAnser(){
+//I need to select the answer and for the background color to stay
+}
+
+function nextQuestion(){
+//I need the browser to randomly select the next questions with answer choices from a math.random 
+}
+
+function timer(){
+//I need a timer, I will have to reserach if it goes on its own function or not
+}
